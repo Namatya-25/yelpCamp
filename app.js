af819b8c6797f71b9e5a7532992085a0ffb4748a
namatya <langhuanamacha@gmail.com>
2026-05-29 13:16:42 +0900

    fix: app.js middleware and port

--- app.js
+++ app.js
@@ -118,7 +118,6 @@ app.use(helmet.contentSecurityPolicy({
 }));
 
 app.use((req, res, next) => {
-    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
